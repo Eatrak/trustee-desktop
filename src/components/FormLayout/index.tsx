dfe26ef0987f34d74d0a775976ce1c92018feb86
Davide Vacirca <davidevacirca8@gmail.com>
2022-06-12 12:06:46 +0200

    chore(form): add form-layout component

---
+++ src/components/FormLayout/index.tsx
@@ -0,0 +1,23 @@
+import "./style.css";
+
+import NormalButton from "../NormalButton";
+
+interface IProps {
+    children: React.ReactNode,
+    submitText: string,
+    submitEvent?: (...p: any) => any
+}
+
+const FormLayout = ({children, submitText, submitEvent}: IProps) => {
+    return(
+        <div className="form">
+            <h3 className="header--bold">Welcome!</h3>
+            <div className="form__fields-container">
+                {children}
+            </div>
+            <NormalButton className="form__submit-button" text={submitText} />
+        </div>
+    );
+}
+
+export default FormLayout;
