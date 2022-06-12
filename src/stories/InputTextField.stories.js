4d0d08140c8dd49be8e53d872531b6ae4cab1eba
Davide Vacirca <davidevacirca8@gmail.com>
2022-06-12 12:09:43 +0200

    chore(input-story): add input-text-field component story

---
+++ src/stories/InputTextField.stories.js
@@ -0,0 +1,17 @@
+import '@styles/themes.css';
+import '../index.css';
+
+import InputTextField from '@components/InputTextField';
+
+export default {
+    title: "InputTextField",
+    component: InputTextField
+};
+
+const Template = args => <InputTextField {...args}/>;
+
+export const Normal = Template.bind({});
+Normal.args = {
+    title: "Email",
+    placeholder: "johndoe@test.com"
+};
