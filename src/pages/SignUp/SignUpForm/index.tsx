b19a389d80f6a15f4e81de5aae1f78ebb2409f08
Davide Vacirca <davidevacirca8@gmail.com>
2022-07-23 10:15:36 +0200

    style(sign-up): use sign-up form as component

---
+++ src/pages/SignUp/SignUpForm/index.tsx
@@ -0,0 +1,47 @@
+import { createRef, RefObject, useState } from "react";
+
+import Validator from "validatorjs";
+
+import FormLayout from "@components/FormLayout";
+import InputTextField from "@components/InputTextField";
+import { signUpValidator } from "@crudValidators/auth";
+import AuthService from "@services/auth";
+
+const SignUpForm = () => {
+    const [submitDisabled, setSubmitDisabled] = useState(true);
+
+    const emailField: RefObject<InputTextField> = createRef();
+    const passwordField: RefObject<InputTextField> = createRef();
+
+    const signUp = async () => {
+        const email = emailField.current!.getValue();
+        const password = passwordField.current!.getValue();
+
+        const successfulSignUp = await AuthService.getInstance().signUp(email, password);
+        if (successfulSignUp) document.location.href = "/sign-in";
+
+        return true;
+    };
+
+    const checkFieldValidity = () => {
+        const email = emailField.current!.getValue();
+        const password = passwordField.current!.getValue();
+
+        const validation = new Validator({ email, password }, signUpValidator);
+
+        validation.fails() ? setSubmitDisabled(true) : setSubmitDisabled(false);
+    };
+    
+    return (
+        <FormLayout submitText="Sign up" submitEvent={signUp} submitDisabled={submitDisabled}>
+            {/* Email field */}
+            <InputTextField ref={emailField} validator={signUpValidator} validatorAttribute="email"
+                title="Email" placeholder="johndoe@test.com" onInput={checkFieldValidity}/>
+            {/* Password field */}
+            <InputTextField ref={passwordField} validator={signUpValidator} validatorAttribute="password"
+                title="Password" onInput={checkFieldValidity}/>
+        </FormLayout>
+    );
+};
+
+export default SignUpForm;
--- src/pages/SignUp/index.tsx
+++ src/pages/SignUp/index.tsx
@@ -1,53 +1,15 @@
 import "./style.css";
 
-import { createRef, useState, RefObject } from "react";
-
-import Validator from "validatorjs";
-
-import FormLayout from '@components/FormLayout';
-import InputTextField from '@components/InputTextField';
 import logo from '@assets/logo.png';
 import { ReactComponent as DotsDecoration } from '@assets/dots-decoration.svg';
-import { signUpValidator } from "@crudValidators/auth";
-import AuthService from "@services/auth";
+import SignUpForm from "./SignUpForm";
 
 const SignUpPage = () => {
-    const [submitDisabled, setSubmitDisabled] = useState(true);
-
-    const emailField: RefObject<InputTextField> = createRef();
-    const passwordField: RefObject<InputTextField> = createRef();
-
-    const signUp = async () => {
-        const email = emailField.current!.getValue();
-        const password = passwordField.current!.getValue();
-
-        const successfulSignUp = await AuthService.getInstance().signUp(email, password);
-        if (successfulSignUp) document.location.href = "/sign-in";
-
-        return true;
-    };
-
-    const checkFieldValidity = () => {
-        const email = emailField.current!.getValue();
-        const password = passwordField.current!.getValue();
-
-        const validation = new Validator({ email, password }, signUpValidator);
-
-        validation.fails() ? setSubmitDisabled(true) : setSubmitDisabled(false);
-    };
-
     return(
         <div className="page page--sign-up">
             <div>
                 <img className="page--sign-up__logo" src={logo} alt="logo"/>
-                <FormLayout submitText="Sign up" submitEvent={signUp} submitDisabled={submitDisabled}>
-                    {/* Email field */}
-                    <InputTextField ref={emailField} validator={signUpValidator} validatorAttribute="email"
-                        title="Email" placeholder="johndoe@test.com" onInput={checkFieldValidity}/>
-                    {/* Password field */}
-                    <InputTextField ref={passwordField} validator={signUpValidator} validatorAttribute="password"
-                        title="Password" onInput={checkFieldValidity}/>
-                </FormLayout>
+                <SignUpForm/>
             </div>
             <div>
                 <DotsDecoration className="page--sign-up__decoration"></DotsDecoration>
