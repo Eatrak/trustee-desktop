c4d4a4f8045d512fc99d30eb3983ce154265f01d
Davide Vacirca <davidevacirca8@gmail.com>
2022-10-07 13:55:00 +0200

    update(transactions-header): add wallet multi-select in transactions header

--- src/sections/TransactionsSection/TransactionsHeader/index.tsx
+++ src/sections/TransactionsSection/TransactionsHeader/index.tsx
@@ -1,11 +1,15 @@
 import "./style.css";
 
-import { MdAdd, MdRefresh } from "react-icons/md";
+import { useState } from "react";
+import { MdAdd, MdOutlineAccountBalanceWallet, MdRefresh } from "react-icons/md";
 
 import RoundedTextIconButton from "@components/RoundedTextIconButton";
 import RoundedIconButton from "@components/RoundedIconButton";
+import MultiSelect from "@components/MultiSelect";
 
 const TransactionsHeader = () => {
+    let [ selectedOptions, setSelectedOptions ] = useState([]);
+
     return (
         <div className="app-layout__header">
             <div className="app-layout__header__texts-container">
@@ -13,6 +17,8 @@ const TransactionsHeader = () => {
                 <p className="paragraph--sub-title">Current month</p>
             </div>
             <div className="app-layout__header__actions-container">
+                <MultiSelect selectedOptions={selectedOptions} setSelectedOptions={setSelectedOptions}
+                    options={[{name: "All", value: "*"}]} Icon={MdOutlineAccountBalanceWallet} text="Wallets"/>
                 <RoundedTextIconButton Icon={MdRefresh}/>
                 <RoundedIconButton Icon={MdAdd}/>
             </div>
