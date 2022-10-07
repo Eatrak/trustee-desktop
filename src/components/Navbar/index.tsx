7a292356bc64ced0d66b72f6f98222fcaaaab410
Davide Vacirca <davidevacirca8@gmail.com>
2022-10-07 13:50:00 +0200

    update(navbar): add transactions module button in navbar

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -1,11 +1,15 @@
 import "./style.css";
 
+import { MdOutlineCreditCard } from "react-icons/md";
+
+import NavbarButton from "./NavbarButton";
 import NavbarUserBadge from "./NavbarUserBadge";
 
 const Navbar = () => {
     return (
         <div className="navbar">
             <div className="navbar__button-container">
+                <NavbarButton path="/" Icon={MdOutlineCreditCard} text="Transactions"/>
             </div>
             <NavbarUserBadge firstName="John" lastName="Doe" email="johndoe@example.com"/>
         </div>
