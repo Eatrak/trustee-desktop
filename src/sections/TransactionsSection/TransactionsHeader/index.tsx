babef3f71757869bfcb9bd6987be02a21d757f6c
Davide Vacirca <davidevacirca8@gmail.com>
2022-10-07 13:46:48 +0200

    feat(transactions-header): add transactions module header component

---
+++ src/sections/TransactionsSection/TransactionsHeader/index.tsx
@@ -0,0 +1,23 @@
+import "./style.css";
+
+import { MdAdd, MdRefresh } from "react-icons/md";
+
+import RoundedTextIconButton from "@components/RoundedTextIconButton";
+import RoundedIconButton from "@components/RoundedIconButton";
+
+const TransactionsHeader = () => {
+    return (
+        <div className="app-layout__header">
+            <div className="app-layout__header__texts-container">
+                <h4 className="header--bold">Monthly transactions</h4>
+                <p className="paragraph--sub-title">Current month</p>
+            </div>
+            <div className="app-layout__header__actions-container">
+                <RoundedTextIconButton Icon={MdRefresh}/>
+                <RoundedIconButton Icon={MdAdd}/>
+            </div>
+        </div>
+    );
+};
+
+export default TransactionsHeader;
