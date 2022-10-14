4a91929dea68143cabaa0a597912eb9b9511f961
Davide Vacirca <davidevacirca8@gmail.com>
2022-10-14 14:07:27 +0200

    feat(transaction-row): add transaction row item

---
+++ src/sections/TransactionsSection/TransactionItem/index.tsx
@@ -0,0 +1,32 @@
+import "./style.css";
+
+import { Transaction } from '@models/transactions';
+import dayjs from "dayjs";
+
+interface IProps {
+    transaction: Transaction
+}
+
+const TransactionItem = ({ transaction }: IProps) => {
+    const getAmountDirectionSymbol = () => {
+        return transaction.isIncome ? "+" : "-";
+    };
+
+    return (
+        <div className="transaction-item">
+            <div className="transaction-item--text-container">
+                <p className="paragraph--regular paragraph--bold">
+                    {transaction.transactionName}
+                </p>
+                <p className="paragraph--small">
+                    {dayjs(transaction.transactionDate).format("MM-DD-YYYY")}
+                </p>
+            </div>
+            <p className="paragraph--regular">
+                {getAmountDirectionSymbol()} {transaction.transactionAmount}€
+            </p>
+        </div>
+    );
+};
+
+export default TransactionItem;
