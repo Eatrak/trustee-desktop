46eb5d4ddcc0d6eb21a7a8f151e7923a17503545
Davide Vacirca <davidevacirca8@gmail.com>
2023-03-15 19:26:26 +0100

    feat(loading-icon): add loading-icon component

---
+++ src/components/LoadingIcon/index.tsx
@@ -0,0 +1,11 @@
+import "./style.css";
+
+import { RiLoader5Fill } from "react-icons/ri";
+
+const LoadingIcon = () => {
+    return (
+        <RiLoader5Fill className="text-button__icon loading-icon"/>
+    );
+};
+
+export default LoadingIcon;
