6d7e2295891e2cc82135719a9853f54473637d09
Davide Vacirca <davidevacirca8@gmail.com>
2023-03-23 21:45:18 +0100

    This is a combination of 2 commits.
    update(transactions): remove wallets multi-select in transactions module
    
    refactor(transactions): remove unused variables

--- src/sections/TransactionsSection/TransactionsHeader/index.tsx
+++ src/sections/TransactionsSection/TransactionsHeader/index.tsx
@@ -1,15 +1,11 @@
 import "./style.css";
 
-import { useState } from "react";
-import { MdAdd, MdOutlineAccountBalanceWallet, MdRefresh } from "react-icons/md";
+import { MdAdd, MdRefresh } from "react-icons/md";
 
 import RoundedTextIconButton from "@components/RoundedTextIconButton";
 import RoundedIconButton from "@components/RoundedIconButton";
-import MultiSelect from "@components/MultiSelect";
 
 const TransactionsHeader = () => {
-    let [ selectedOptions, setSelectedOptions ] = useState([]);
-
     return (
         <div className="app-layout__header">
             <div className="app-layout__header__texts-container">
@@ -17,8 +13,6 @@ const TransactionsHeader = () => {
                 <p className="paragraph--sub-title">Current month</p>
             </div>
             <div className="app-layout__header__actions-container">
-                <MultiSelect selectedOptions={selectedOptions} setSelectedOptions={setSelectedOptions}
-                    options={[{name: "All", value: "*"}]} Icon={MdOutlineAccountBalanceWallet} text="Wallets"/>
                 <RoundedTextIconButton Icon={MdRefresh}/>
                 <RoundedIconButton Icon={MdAdd}/>
             </div>
