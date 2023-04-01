9aace0ae2bc0e30e36f54ff3b68c4c3c1642914f
Davide Vacirca <davidevacirca8@gmail.com>
2023-04-01 21:25:21 +0200

    feat: show date-picker used to choose the transactions to show based on the creation date

--- src/sections/TransactionsSection/TransactionsHeader/index.tsx
+++ src/sections/TransactionsSection/TransactionsHeader/index.tsx
@@ -1,11 +1,19 @@
 import "./style.css";
 
-import { MdAdd, MdRefresh } from "react-icons/md";
+import { useState } from "react";
+import { MdAdd, MdDateRange, MdRefresh } from "react-icons/md";
 
 import RoundedTextIconButton from "@components/RoundedTextIconButton";
 import RoundedIconButton from "@components/RoundedIconButton";
+import DatePicker from "@components/DatePicker";
 
 const TransactionsHeader = () => {
+    let [isDatePickerOpened, changeIsDatePickerOpened] = useState<boolean>(false);
+
+    const openDatePicker = () => {
+        changeIsDatePickerOpened(true);
+    };
+
     return (
         <div className="app-layout__header">
             <div className="app-layout__header__texts-container">
@@ -14,6 +22,15 @@ const TransactionsHeader = () => {
             </div>
             <div className="app-layout__header__actions-container">
                 <RoundedTextIconButton Icon={MdRefresh}/>
+                <RoundedTextIconButton
+                    Icon={MdDateRange}
+                    clickEvent={openDatePicker}/>
+                {
+                    isDatePickerOpened &&
+                    <DatePicker
+                        setOpened={changeIsDatePickerOpened}
+                        style={{ left: "50%", top: "50px", transform: "translate(-50%)" }}/>
+                }
                 <RoundedIconButton Icon={MdAdd}/>
             </div>
         </div>
