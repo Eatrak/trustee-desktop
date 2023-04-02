b97dc78e26757267041133b05e46324c3e172fc1
Davide Vacirca <davidevacirca8@gmail.com>
2023-04-02 20:47:51 +0200

    update(date-picker): allow to select range for multiple months in date-picker

--- src/components/DatePicker/index.tsx
+++ src/components/DatePicker/index.tsx
@@ -2,7 +2,7 @@ import "./style.css";
 
 import { useEffect, useState, useRef } from "react";
 import { MdChevronLeft, MdChevronRight } from "react-icons/md";
-import dayjs from "dayjs";
+import dayjs, { Dayjs } from "dayjs";
 
 import MiniRoundedIconButton from "@components/MiniRoundedIconButton";
 
@@ -17,9 +17,9 @@ const DatePicker = ({ style, setOpened }: IProps) => {
     const dayNames = ["Mo", "Tu", "We", "Th", "Fr", "Sat", "Su"];
     const currentYearAndMonth = dayjs().format("YYYY-MM");
     let [selectedYearAndMonth, changeYearAndMonth] = useState<string>(currentYearAndMonth);
-    let [startDay, changeStartDay] = useState<number | null>(null);
-    let [endDay, changeEndDay] = useState<number | null>(null);
-    let [endDayCandidate, changeEndDayCandidate] = useState<number | null>();
+    let [startDate, changeStartDate] = useState<Dayjs | null>(null);
+    let [endDate, changeEndDate] = useState<Dayjs | null>(null);
+    let [endDateCandidate, changeEndDateCandidate] = useState<Dayjs | null>();
 
     const selectPreviousMonth = () => {
         const previousMonthYear = dayjs(selectedYearAndMonth).subtract(1, "month");
@@ -31,33 +31,33 @@ const DatePicker = ({ style, setOpened }: IProps) => {
         changeYearAndMonth(nextMonthYear.format());
     };
 
-    const getMonthDayCSSClass = (day: number) => {
+    const getMonthDayCSSClass = (date: Dayjs) => {
         let cssClass = "date-picker__month-day-list__month-day";
 
         // If no days have been selected, all the days are start-day candidates
-        if (startDay == null && endDay == null) {
+        if (startDate == null && endDate == null) {
             cssClass += " date-picker__month-day-list__month-day--start-day-candidate";
         }
         
         // If the start-day has been selected
-        if (startDay != null) {
+        if (startDate != null) {
             // If the days range has been selected, the days out of the range are start-day candidates
-            if (endDay != null && (day < startDay || day > endDay)) {
+            if (endDate != null && (date.isBefore(startDate) || date.isAfter(endDate))) {
                 cssClass += " date-picker__month-day-list__month-day--start-day-candidate";
             }
 
-            const isDayInCandidateRange = day > startDay && endDayCandidate != null && endDay == null && day < endDayCandidate;
-            const isDayInRange = day > startDay && endDay != null && day < endDay;
+            const isDateInCandidateRange = date.isAfter(startDate) && endDateCandidate != null && endDate == null && date.isBefore(endDateCandidate);
+            const isDateInRange = date.isAfter(startDate) && endDate != null && date.isBefore(endDate);
             // If the day is in the candidate range or in the selected range
-            if (isDayInCandidateRange || isDayInRange) {
+            if (isDateInCandidateRange || isDateInRange) {
                 cssClass += " date-picker__month-day-list__month-day--selected-range-day";
             }
 
-            const isDayTheEndDayCandidate = endDayCandidate != null && endDay == null && day == endDayCandidate;
-            const isDayTheStartDay = day == startDay;
-            const isDayTheEndDay = endDay != null && day == endDay;
+            const isDateTheEndDateCandidate = endDateCandidate != null && endDate == null && date.isSame(endDateCandidate);
+            const isDateTheStartDate = date.isSame(startDate);
+            const isDateTheEndDate = endDate != null && date.isSame(endDate);
             // If the day is an extreme of the candidate range or selected range
-            if (isDayTheStartDay || isDayTheEndDayCandidate || isDayTheEndDay) {
+            if (isDateTheStartDate || isDateTheEndDateCandidate || isDateTheEndDate) {
                 cssClass += " date-picker__month-day-list__month-day--selected-range-extreme-day";
             }
         }
@@ -65,17 +65,17 @@ const DatePicker = ({ style, setOpened }: IProps) => {
         return cssClass;
     };
 
-    const changeInterval = (day: number) => {
-        if ((startDay == null && endDay == null) || (startDay != null && day < startDay)) {
-            changeStartDay(day);
+    const changeInterval = (date: Dayjs) => {
+        if ((startDate == null && endDate == null) || (startDate != null && date.isBefore(startDate))) {
+            changeStartDate(date);
         }
-        else if (startDay != null && endDay == null) {
-            changeEndDay(day);
+        else if (startDate != null && endDate == null) {
+            changeEndDate(date);
         }
 
-        if (startDay != null && endDay != null) {
-            changeStartDay(day);
-            changeEndDay(null);
+        if (startDate != null && endDate != null) {
+            changeStartDate(date);
+            changeEndDate(null);
         }
     };
 
@@ -121,11 +121,13 @@ const DatePicker = ({ style, setOpened }: IProps) => {
                 <div className="date-picker__month-day-list">
                     {
                         [...Array(dayjs(selectedYearAndMonth).daysInMonth()).keys()].map(day => {
+                            const date = dayjs(selectedYearAndMonth).set("day", day + 1);
+                            
                             return (
                                 <div
-                                    className={getMonthDayCSSClass(day + 1)}
-                                    onClick={() => changeInterval(day + 1)}
-                                    onMouseEnter={() => changeEndDayCandidate(day + 1)}>
+                                    className={getMonthDayCSSClass(date)}
+                                    onClick={() => changeInterval(date)}
+                                    onMouseEnter={() => changeEndDateCandidate(date)}>
 
                                     <p key={day + 1} className="paragraph--small paragraph--bold date-picker__month-day-list__month-day__text">
                                         {day + 1}
