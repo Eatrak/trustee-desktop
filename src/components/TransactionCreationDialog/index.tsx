13a805e7bcd48914abf2004aad62347020b148c1
Davide Vacirca <davidevacirca8@gmail.com>
2023-05-01 22:13:24 +0200

    update(transaction-dialog): add possibility to select wallet currency and to create wallets in transaction-creation-dialog

--- src/components/TransactionCreationDialog/index.tsx
+++ src/components/TransactionCreationDialog/index.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { RefObject, useEffect, useRef, useState } from "react";
 import { MdAdd } from "react-icons/md";
 import Validator from "validatorjs";
 import { Dayjs } from "dayjs";
@@ -6,22 +6,27 @@ import { Dayjs } from "dayjs";
 import "./style.css";
 import Dialog from "@components/Dialog";
 import InputTextField from "@components/InputTextField";
-import { Wallet } from "@models/transactions";
+import { Currency, Wallet } from "@models/transactions";
 import TransactionsService from "@services/transactions";
 import NormalButton from "@components/NormalButton";
 import TextButton from "@components/TextButton";
 import Select, { SelectOption } from "@components/Select";
 import DatePicker from "@components/DatePicker";
+import MiniSelect from "@components/MiniSelect";
 import { createTransactionRules } from "@crudValidators/transactions";
 
 const TransactionCreationDialog = () => {
+    let currencySelect = useRef<React.ElementRef<typeof MiniSelect>>(null);
     let [isSubmitEnabled, setIsSubmitEnabled] = useState<boolean>(false);
     let [ wallets, setWallets ] = useState<Wallet[]>([]);
+    let [ currencies, setCurrencies ] = useState<Currency[]>([]);
     let [isDatePickerOpened, setIsDatePickerOpened] = useState<boolean>(false);
+    let [ isCreatingNewWallet, setIsCreatingNewWallet ] = useState<boolean>(false);
 
     // Form data
     let [ name, setName ] = useState<string>();
     let [ walletOption, setWalletOption ] = useState<SelectOption>();
+    let [ selectedNewWalletCurrency, setSelectedNewWalletCurrency ] = useState<SelectOption>();
     let [ categoryOption, setCategoryOption ] = useState<SelectOption>();
     let [ creationDate, setCreationDate ] = useState<Dayjs>();
     let [ value, setValue ] = useState<string>();
@@ -30,6 +35,13 @@ const TransactionCreationDialog = () => {
         return wallets.map(({ walletId, walletName }) => ({
             name: walletName,
             value: walletId
+        }));
+    };
+
+    const getCurrencyOptions = (): SelectOption[] => {
+        return currencies.map(({ currencyCode, currencySymbol }) => ({
+            name: `${currencySymbol} ${currencyCode}`,
+            value: currencyCode
         }));
     };
 
@@ -45,15 +57,35 @@ const TransactionCreationDialog = () => {
         return validator.passes() || false;
     };
 
+    const createWallet = async (newWalletName: string) => {
+        setIsCreatingNewWallet(true);
+        await TransactionsService.getInstance().createWallet({
+            walletName: newWalletName,
+            currencyCode: selectedNewWalletCurrency!.value
+        });
+        setIsCreatingNewWallet(false);
+    };
+
     useEffect(() => {
         TransactionsService.getInstance().wallets$.subscribe(setWallets);
+        TransactionsService.getInstance().currencies$.subscribe((currencies) => {
+            setCurrencies(currencies);
+
+            // Set default currency option
+            const { currencyCode, currencySymbol } = currencies[0];
+            currencySelect.current?.setSelectedOption({
+                name: `${currencySymbol} ${currencyCode}`,
+                value: currencyCode
+            });
+        });
 
         TransactionsService.getInstance().getWallets();
+        TransactionsService.getInstance().getCurrencies();
     }, []);
 
     useEffect(() => {
         setIsSubmitEnabled(isFormValid());
-    }, [ name, walletOption, categoryOption, creationDate, value ]);
+    }, [ name, walletOption, selectedNewWalletCurrency, categoryOption, creationDate, value ]);
 
     return (
         <Dialog title="Transaction creation">
@@ -70,8 +102,19 @@ const TransactionCreationDialog = () => {
                     text="Wallet"
                     filterInputPlaceholder="Search or create by typing a name"
                     options={getWalletOptions()}
+                    createNewOption={createWallet}
+                    isCreatingNewOption={isCreatingNewWallet}
+                    getCreateNewOptionButtonText={(nameOfWalletToCreate) => `Create "${nameOfWalletToCreate}" wallet`}
                     validatorRule="required"
-                    onSelect={setWalletOption} />
+                    onSelect={setWalletOption}
+                >
+                    <MiniSelect
+                        ref={currencySelect}
+                        className="currency-select"
+                        options={getCurrencyOptions()}
+                        entityName="currency"
+                        onSelect={setSelectedNewWalletCurrency} />
+                </Select>
                 {/* Category */}
                 <Select
                     entityName="category"
