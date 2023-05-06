5aa439c8030cc1ae90e03006c88c0a0112d6498b
Davide Vacirca <davidevacirca8@gmail.com>
2023-05-06 09:59:00 +0200

    refactor(checkbox): add possibility to add text in checkbox component

--- src/components/Checkbox/index.tsx
+++ src/components/Checkbox/index.tsx
@@ -3,17 +3,24 @@ import "./style.css";
 import { MdDone } from "react-icons/md";
 
 interface IProps {
+    className?: string,
+    text?: string,
     checked: boolean,
     setChecked: (...p: any) => any
 }
 
-const Checkbox = ({ checked, setChecked }: IProps) => {
+const Checkbox = ({ className, text, checked, setChecked }: IProps) => {
     return (
-        <div className="checkbox" onClick={e => setChecked(!checked)}>
-            <input className="checkbox__input" type="checkbox"/>
-            {checked && <div className="checkbox__checked-box">
-                <MdDone className="checkbox__checked-box__icon"/>
-            </div>}
+        <div className={`checkbox-with-text ${className ? className : ""}`}>
+            {text && <p className="paragraph--small">{text}</p>}
+            <div
+                className="checkbox"
+                onClick={e => setChecked(!checked)}
+            >
+                {checked && <div className="checkbox__checked-box">
+                    <MdDone className="checkbox__checked-box__icon"/>
+                </div>}
+            </div>
         </div>
     );
 };
