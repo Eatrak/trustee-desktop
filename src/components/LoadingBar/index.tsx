eab0fa5f10057ebf4bd828d7481a9e2533345e29
Davide Vacirca <davidevacirca8@gmail.com>
2023-05-27 11:17:03 +0200

    feat: add loading-bar component

---
+++ src/components/LoadingBar/index.tsx
@@ -0,0 +1,11 @@
+import "./style.css";
+
+const LoadingBar = () => {
+    return (
+        <div className="loading-bar">
+            <div className="loading-bar__bar"></div>
+        </div>
+    );
+};
+
+export default LoadingBar;
