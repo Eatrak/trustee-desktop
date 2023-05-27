ca8a57dbcd027340936eb2bc75bdb8d6aacde121
Davide Vacirca <davidevacirca8@gmail.com>
2023-05-27 11:17:19 +0200

    feat: add loading-page

---
+++ src/pages/LoadingPage/index.tsx
@@ -0,0 +1,16 @@
+import "./style.css";
+import logo from "../../assets/logo.jpg";
+import LoadingBar from "@components/LoadingBar";
+
+const LoadingPage = () => {
+    return (
+        <div className="page loading-page">
+            <div className="loading-page__content">
+                <img src={logo} draggable={false} />
+                <LoadingBar />
+            </div>
+        </div>
+    );
+};
+
+export default LoadingPage;
