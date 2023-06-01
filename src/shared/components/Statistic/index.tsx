f00152c00cd1c75936834af10bd138f8f300a093
Davide Vacirca <davidevacirca8@gmail.com>
2023-06-01 20:23:23 +0200

    feat: add statistic component

---
+++ src/shared/components/Statistic/index.tsx
@@ -0,0 +1,17 @@
+interface IProps {
+    title: string,
+    value: string,
+    size?: "normal" | "large"
+}
+
+const Statistic = ({ title, value, size = "normal" }: IProps) => {
+    return (
+        <div className="statistic">
+            <p className="paragraph paragraph--sub-title">{title}</p>
+            {size == "large" && <h6 className="header--semi-bold">{value}</h6>}
+            {size == "normal" && <p className="paragraph paragraph--large paragraph--semi-bold">{value}</p>}
+        </div>
+    );
+};
+
+export default Statistic;
