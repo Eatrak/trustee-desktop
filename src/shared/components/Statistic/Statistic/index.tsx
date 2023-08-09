c67cd3083479b058c383dba17ff0295b1937c623
Davide Vacirca <davidevacirca8@gmail.com>
2023-08-09 22:47:55 +0200

    feat: show tooltip in statistic component

--- src/shared/components/Statistic/Statistic/index.tsx
+++ src/shared/components/Statistic/Statistic/index.tsx
@@ -1,3 +1,6 @@
+import { Tooltip } from "react-tooltip";
+import { v4 as uuid } from "uuid";
+
 import "./style.css";
 
 interface IProps {
@@ -8,12 +11,34 @@ interface IProps {
 }
 
 const Statistic = ({ title, value, size = "normal", className }: IProps) => {
+    const tooltipId = uuid();
+
     return (
         <div className={`statistic ${className || ""}`}>
             <p className="statistic__title paragraph paragraph--sub-title">{title}</p>
-            {size == "large" && <h6 className="header--semi-bold">{value}</h6>}
+            {size == "large" && (
+                <>
+                    <h6
+                        data-tooltip-id={tooltipId}
+                        data-tooltip-content={value}
+                        className="statistic__value header--semi-bold"
+                    >
+                        {value}
+                    </h6>
+                    <Tooltip id={tooltipId} />
+                </>
+            )}
             {size == "normal" && (
-                <p className="paragraph paragraph--large paragraph--semi-bold">{value}</p>
+                <>
+                    <p
+                        data-tooltip-id={tooltipId}
+                        data-tooltip-content={value}
+                        className="statistic__value paragraph paragraph--large paragraph--semi-bold"
+                    >
+                        {value}
+                    </p>
+                    <Tooltip id={tooltipId} />
+                </>
             )}
         </div>
     );
