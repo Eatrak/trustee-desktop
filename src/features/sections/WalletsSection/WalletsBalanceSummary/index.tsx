32d52b1fdddd5fd017b86e9238013b07bdd51ae4
Davide Vacirca <davidevacirca8@gmail.com>
2023-08-13 23:12:27 +0200

    feat: add wallets-balance-summary component

---
+++ src/features/sections/WalletsSection/WalletsBalanceSummary/index.tsx
@@ -0,0 +1,63 @@
+import { FC } from "react";
+
+import "./style.css";
+import { Utils } from "@shared/services/utils";
+import StatisticSkeleton from "@shared/components/Statistic/StatisticSkeleton";
+import Statistic from "@shared/components/Statistic/Statistic";
+
+interface IProps {
+    isLoading?: boolean;
+    currencyCode: string;
+    totalIncome: number;
+    totalExpense: number;
+}
+
+const WalletsBalanceSummary: FC<IProps> = ({
+    isLoading = false,
+    currencyCode,
+    totalIncome,
+    totalExpense,
+}) => {
+    const getFormattedAmount = (amount: number) => {
+        return Utils.getInstance().getFormattedAmount(currencyCode, amount);
+    };
+
+    return (
+        <div className="card wallets-balance-summary">
+            <div className="wallets-balance-summary__left">
+                {isLoading ? (
+                    <StatisticSkeleton title="Total income" width="180px" />
+                ) : (
+                    <Statistic
+                        className="wallets-balance-summary__left__total-income"
+                        title="Total income"
+                        value={getFormattedAmount(totalIncome)}
+                    />
+                )}
+                {isLoading ? (
+                    <StatisticSkeleton title="Total expense" width="180px" />
+                ) : (
+                    <Statistic
+                        className="wallets-balance-summary__left__total-expense"
+                        title="Total expense"
+                        value={getFormattedAmount(totalExpense)}
+                    />
+                )}
+            </div>
+            <div className="wallets-balance-summary__right">
+                {isLoading ? (
+                    <StatisticSkeleton title="Total net" width="180px" size="large" />
+                ) : (
+                    <Statistic
+                        className="wallets-balance-summary__right__total-balance"
+                        title="Total net"
+                        value={getFormattedAmount(totalIncome - totalExpense)}
+                        size="large"
+                    />
+                )}
+            </div>
+        </div>
+    );
+};
+
+export default WalletsBalanceSummary;
