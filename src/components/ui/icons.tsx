9c80deca847641aef6077e5d50ce77320a5ebf6d
Davide Vacirca <davidevacirca8@gmail.com>
2023-11-26 11:07:06 +0100

    feat: add loading page

--- src/components/ui/icons.tsx
+++ src/components/ui/icons.tsx
@@ -1,24 +1,20 @@
-import { cn } from "@/lib/utils";
-
 interface IProps {
     className?: string;
 }
 
-export const LoadingSpinner = ({ className }: IProps) => {
-    return (
+export const Icons = {
+    loading: (props: IProps) => (
         <svg
             xmlns="http://www.w3.org/2000/svg"
-            width="32"
-            height="32"
             viewBox="0 0 24 24"
             fill="none"
             stroke="currentColor"
             strokeWidth="2"
             strokeLinecap="round"
             strokeLinejoin="round"
-            className={cn("animate-spin", className)}
+            {...props}
         >
             <path d="M21 12a9 9 0 1 1-6.219-8.56" />
         </svg>
-    );
+    ),
 };
--- src/shared/customComponents/LoadingPage/index.tsx
+++ src/shared/customComponents/LoadingPage/index.tsx
@@ -1,11 +1,11 @@
 import "./style.css";
-import { LoadingSpinner } from "@/components/ui/loadingSpinner";
+import { Icons } from "@/components/ui/icons";
 
 const LoadingPage = () => {
     return (
         <div className="page loading-page">
             <div className="loading-content">
-                <LoadingSpinner />
+                <Icons.loading className="w-8 h-8 animate-spin" />
             </div>
         </div>
     );
